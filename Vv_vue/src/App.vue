9ae5d6d7652517dc21e5e9f6a071cae14dcdc7c8
Woobzor <vladychin.v.i.2.21@gmail.com>
2023-02-08 19:35:36 +0300

    Выполнения кода в представлениях Vue

--- Vv_vue/src/App.vue
+++ Vv_vue/src/App.vue
@@ -3,20 +3,30 @@ export default {
   name: "App",
   data() {
     return {
-      text: "page",
-      href: "page.html",
+      //Задание №1
+      num: 5,
+
+      //Задание №2
+      num1: 1,
+      num2: 2,
+      num3: 3,
     };
   },
 };
 </script>
 
 <template>
-  <h1>Задание №5</h1>
-
+  <h1>Упражнение №6</h1>
   <br />
 
   <div>
-    <a :href="href">{{ text }}</a>
+    <!-- Задание №1  -->
+    <p>Задание №1. Квадрат из числа 5 = {{ num ** 2 }}</p>
+
+    <br />
+
+    <!-- Задание №2 -->
+    <p>Задание №2. Сумма чисел 1, 2, 3 равна {{ num1 + num2 + num3 }}</p>
   </div>
   <main>
     <TheWelcome />
