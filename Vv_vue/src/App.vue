a860eafb7ae8cfa469b8968d8b7f556e09a55abd
Woobzor <vladychin.v.i.2.21@gmail.com>
2023-02-22 20:51:36 +0000

    Реактивность массивов

--- Vv_vue/src/App.vue
+++ Vv_vue/src/App.vue
@@ -3,41 +3,68 @@ export default {
   name: "App",
   data() {
     return {
-      //Задание №1
-      products: [
-        {
-          id: 1,
-          name: "product1",
-        },
-        {
-          id: 2,
-          name: "product2",
-        },
-        {
-          id: 3,
-          name: "product3",
-        },
-      ],
+      //Задание №1, №2, №3, №4, №5, №6
+      arr: ["a", "b", "c", "d", "e", "f"],
     };
   },
 
   methods: {
     // Задание №1
+    add: function () {
+      this.arr.push("g");
+    },
+    // Задание №2
+    delf: function () {
+      this.arr.shift();
+    },
+    // Задание №3
+    dell: function () {
+      this.arr.pop();
+    },
+    // Задание №4
+    splice: function () {
+      this.arr.splice(2, 1);
+    },
+    // Задание №5
+    sort: function () {
+      this.arr.sort();
+    },
+    // Задание №6
+    reverse: function () {
+      this.arr.reverse();
+    },
   },
 };
 </script>
 
 <template>
-  <h1>Упражнение №34</h1>
+  <h1>Упражнение №35</h1>
   <br />
 
   <!-- Задание №1 -->
-  <h2>Задание №1</h2>
+  <h2>Задания №1, №2, №3, №4, №5, №6</h2>
   <br />
 
-  <ul class="ul" v-for="product in products" :key="product.id">
-    <li>{{ product.name }}</li>
+  <!-- Задание №1 -->
+  <ul class="ul">
+    <li v-for="elem in arr">{{ elem }}</li>
   </ul>
+  <button @click="add">add</button>
+
+  <!-- Задание №2 -->
+  <button @click="delf">del first</button>
+
+  <!-- Задание №3 -->
+  <button @click="dell">del last</button>
+
+  <!-- Задание №4 -->
+  <button @click="splice">del penult</button>
+
+  <!-- Задание №5 -->
+  <button @click="sort">sort</button>
+
+  <!-- Задание №6 -->
+  <button @click="reverse">reverse order</button>
 
   <main>
     <TheWelcome />
