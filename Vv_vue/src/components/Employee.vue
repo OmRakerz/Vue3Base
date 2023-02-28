84411b96b4d9a4cfbeace59ad7b23503ccada74e
Woobzor <vladychin.v.i.2.21@gmail.com>
2023-02-28 15:59:49 +0000

    Создание дочерних компонентов

---
+++ Vv_vue/src/components/Employee.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      //Задание №1
+      name: "Valera",
+    };
+  },
+
+  methods: {
+    //Задание №1
+  },
+};
+</script>
+
+<template>
+  <!-- Задание №1 -->
+
+  <p>{{ name }}</p>
+</template>
+
+<style></style>
