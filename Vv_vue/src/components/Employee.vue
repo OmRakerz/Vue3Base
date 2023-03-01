68deb017ee2d4a4300483316798ede01d92a1632
Woobzor <vladychin.v.i.2.21@gmail.com>
2023-03-01 17:43:27 +0000

    Испускание событий

--- Vv_vue/src/components/Employee.vue
+++ Vv_vue/src/components/Employee.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-  props: {},
+  props: { show1: String, show2: String },
 
   data() {
     return {
@@ -10,12 +10,21 @@ export default {
 
   methods: {
     //Задание №1
+    showApp1: function () {
+      this.$emit("show1", "xxx");
+    },
+
+    showApp2: function () {
+      this.$emit("show2", "yyy");
+    },
   },
 };
 </script>
 
 <template>
   <!-- Задание №1 -->
+  <button class="button" @click="showApp1">One func</button>
+  <button class="button" @click="showApp2">Two func</button>
 </template>
 
 <style></style>
